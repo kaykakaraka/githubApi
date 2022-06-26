7017f3c5c859a77aa39e695284822eb1d8fe7766
Kay <kard@hotmail.co.uk>
2022-06-26 14:04:05 +0100

    added github api file

---
+++ githubApi.js
@@ -0,0 +1,29 @@
+// Implement a class GithubApi (in githubApi.js) with a method fetchRepositoryData which:
+// fetches repository data from Github's API.
+// calls the given callback with the received data (after transforming the JSON into a JavaScript object).
+// // In node
+
+// const GithubApi = require('./githubApi');
+
+// const api = new GithubApi();
+
+// api.fetchRepositoryData('sinatra/sinatra', (repositoryData) => {
+//   console.log(repositoryData);
+// });
+
+// // This should log:
+// //
+// // {
+// //   id: 106995,
+// //   node_id: 'MDEwOlJlcG9zaXRvcnkxMDY5OTU=',
+// //   name: 'sinatra',
+// //   full_name: 'sinatra/sinatra',
+// //   private: false,
+// //   owner: {
+// //     login: 'sinatra',
+// //     id: 8312,
+// //     node_id: 'MDEyOk9yZ2FuaXphdGlvbjgzMTI=',
+// //   (...)
+// //   (...)
+// // }
+
